efe9d4ba2d985e4d54af92008679db6498837268
Viktor Sarström <viktorsarstrom@gmail.com>
2024-10-30 10:39:11 +0100

    Remove formatDirectoryEntry function

--- src/features/terminal/types/format-directory-entry.ts
+++
@@ -1,8 +0,0 @@
-export const formatDirectoryEntry = (
-	name: string,
-	type: "directory" | "file",
-): string => {
-	return type === "directory"
-		? `<span class="font-semibold">${name}/</span>`
-		: `<span class="opacity-90">${name}</span>`;
-};
