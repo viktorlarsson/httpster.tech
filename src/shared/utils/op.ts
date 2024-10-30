78dc4e8d9cdace51c90e2c379cfda046ab10c997
Viktor Sarström <viktorsarstrom@gmail.com>
2024-10-30 12:56:10 +0100

    Update OpenPanel clientId with hardcoded value

--- src/shared/utils/op.ts
+++ src/shared/utils/op.ts
@@ -1,7 +1,8 @@
 import { OpenPanel } from "@openpanel/web";
 
 export const op = new OpenPanel({
-	clientId: process.env.PUBLIC_OPENPANEL_PK || "",
+	// TODO: fix this https://github.com/oven-sh/bun/issues/9877
+	clientId: "4658a009-c860-4a67-b499-69bb1b4f8de0",
 	trackScreenViews: true,
 	trackOutgoingLinks: true,
 	trackAttributes: true,
