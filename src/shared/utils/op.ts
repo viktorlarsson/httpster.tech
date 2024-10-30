21df3fb371e0ecee001ff6969b4dd16bedb15670
Viktor Sarström <viktorsarstrom@gmail.com>
2024-10-30 12:39:17 +0100

    Use env variable for OpenPanel clientId

--- src/shared/utils/op.ts
+++ src/shared/utils/op.ts
@@ -1,7 +1,7 @@
 import { OpenPanel } from "@openpanel/web";
 
 export const op = new OpenPanel({
-	clientId: "4658a009-c860-4a67-b499-69bb1b4f8de0",
+	clientId: import.meta.env.PUBLIC_OPENPANEL_PK || "",
 	trackScreenViews: true,
 	trackOutgoingLinks: true,
 	trackAttributes: true,
